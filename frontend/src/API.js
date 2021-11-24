14439d611c1265a974e7219b000258bb081b589c
samsaddam <samsaddam14@gmail.com>
2021-11-24 22:26:50 +0530

    added database

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -10,7 +10,7 @@ var baseURL;
 // } else 
 // baseURL = "http://127.0.0.1:8000";
 // }
-baseURL = "http://127.0.0.1:8000";
+baseURL = "https://lemonade-backend.herokuapp.com/";
 
 const api = axios.create({
   baseURL: baseURL,
